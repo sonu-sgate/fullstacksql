1fce65851b7b93aeade9e5d9b50e5e55f639147d
Sonu sGate <sonu@sgate.net>
2023-12-04 17:19:52 +0530

    working on report system

--- ServerSide/Routes/EmpActivityRouter.js
+++ ServerSide/Routes/EmpActivityRouter.js
@@ -6,7 +6,7 @@ empActivityrouter.get("/profile",async(req,res)=>{
     const {email}=req.body
     try{
 const [joindata]=await connection.promise().query(`SELECT * FROM employ INNER JOIN category ON employ.category_id=category.id Where email='${email}'`)
-console.log(joindata,"joindataprofile")
+// console.log(joindata,"joindataprofile")
 res.status(200).json({profile:joindata[0]})
     }catch(err){
         res.status(400).json({msg:"Something going wrong"})
