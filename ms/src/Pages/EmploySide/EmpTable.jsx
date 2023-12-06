be9a8db4d216349c8848d827a28605abd0673f19
Sonu sGate <sonu@sgate.net>
2023-12-06 18:52:47 +0530

    changes to frontend of chatapp

--- ms/src/Pages/EmploySide/EmpTable.jsx
+++ ms/src/Pages/EmploySide/EmpTable.jsx
@@ -1,139 +1,73 @@
+import React, { useState } from "react";
 import {
   Flex,
   Stack,
-  SimpleGrid,
   chakra,
   useColorModeValue,
   Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  TableContainer,
+  Table,
+  TableCaption,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Tfoot,
+  Td,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  Text,
+  SimpleGrid,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { ChatIcon } from "@chakra-ui/icons";
 
-const EmpsideTable = ({id,name,cat}) => {
+const EmpTable = ({ id, name, cat,hanldechatid }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedPerson, setSelectedPerson] = useState(null);
 
-  
   const dataColor = useColorModeValue("white", "gray.800");
   const bg = useColorModeValue("white", "gray.800");
   const bg2 = useColorModeValue("gray.100", "gray.700");
 
-  const handleDelete = (index) => {
-    const newData = [...data];
-    newData.splice(index, 1);
-    setData(newData);
+  const handleDelete = (personIndex) => {
+    setSelectedPerson(personIndex);
+    onOpen();
   };
-  const handleScrollToPersonList = () => {
-    if (personListRef.current) {
-      personListRef.current.scrollIntoView({ behavior: "smooth" });
-    }
+
+  const confirmDelete = () => {
+    // Implement your delete logic here
+    // For this example, I'll just close the modal
+    onClose();
   };
+
+  const scrollToBottom = (id) => {
+    hanldechatid(id)
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
   return (
-    <Flex
-      w="full"
-      bg="#edf3f8"
-      _dark={{
-        bg: "#3e3e3e",
-      }}
-      p={50}
-      alignItems="center"
-      justifyContent="center"
-    >
-      <Stack
-        direction={{
-          base: "column",
-        }}
-        w="full"
-        bg={{
-          md: bg,
-        }}
-        shadow="lg"
-      >
-        <Flex
-          direction={{
-            base: "row",
-            md: "column",
-          }}
-          bg={dataColor}
-          key={id}
-        >
-          <SimpleGrid
-            spacingY={3}
-            columns={{
-              base: 1,
-              md: 3,
-            }}
-            w={{
-              base: 120,
-              md: "full",
-            }}
-            textTransform="uppercase"
-            bg={bg2}
-            color={"gray.500"}
-            py={{
-              base: 1,
-              md: 4,
-            }}
-            px={{
-              base: 2,
-              md: 10,
-            }}
-            fontSize="md"
-            fontWeight="hairline"
-          >
-            <span>EmpId</span>
-            <span>Name</span>
-            <span>Category</span>
-            <chakra.span
-              textAlign={{
-                md: "right",
-              }}
-            >
-              Connect to:-
-            </chakra.span>
-          </SimpleGrid>
-          <SimpleGrid
-            spacingY={3}
-            columns={{
-              base: 1,
-              md: 3,
-            }}
-            w="full"
-            py={2}
-            px={10}
-            fontWeight="hairline"
-          >
-            <span>{id}</span>
-            <chakra.span
-              textOverflow="ellipsis"
-              overflow="hidden"
-              whiteSpace="nowrap"
-            >
-              {name}
-            </chakra.span>
-            <chakra.span
-              textOverflow="ellipsis"
-              overflow="hidden"
-              whiteSpace="nowrap"
-            >
-              {cat}
-            </chakra.span>
-            <Flex
-              justify={{
-                md: "end",
-              }}
-            >
-              <Button
-                variant="solid"
-                colorScheme="red"
-                size="sm"
-                onClick={() => handleDelete(pid)}
-              >
-               Click
-              </Button>
-            </Flex>
-          </SimpleGrid>
-        </Flex>
-      </Stack>
-    </Flex>
+    <Tbody key={id}>
+      <Tr>
+        <Td>{id}</Td>
+        <Td>{name}</Td>
+        <Td >{cat}</Td>
+        <Td>
+          <Button onClick={()=>scrollToBottom(id)}>
+            Chat <ChatIcon />
+          </Button>
+        </Td>
+      </Tr>
+    </Tbody>
   );
 };
 
-export default EmpsideTable;
+export default EmpTable;
--- ms/src/Pages/EmploySide/empHome/ChatApp.jsx
+++ ms/src/Pages/EmploySide/empHome/ChatApp.jsx
@@ -18,6 +18,7 @@ import {
   Tbody,
   Td,
   Tr,
+  TableContainer,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import io from "socket.io-client";
@@ -29,6 +30,7 @@ import axios from "axios";
 import { ChatIcon } from "@chakra-ui/icons";
 import { getempside } from "../../../Redux/Authenticaton/Employee/GetEmp/Action";
 import EmpsideTable from "../EmpTable";
+import EmpTable from "../EmpTable";
 
 const MotionBox = motion(Box);
 
@@ -94,95 +96,116 @@ const ChatApp = () => {
       });
     }
   };
+  const hanldechatid=(id)=>{
+    setRecieverId(id)
+    // console.log("id",id)
+  }
   // console.log("data", data);
   return (
-    <ChakraProvider>
-    
-      <Box >
-        {data &&
-          data.length > 0 &&
-          data.map((item, index) => (
-            <EmpsideTable
-              id={item.empid}
-              name={item.name}
-              cat={item.categoryname}
-            />
-          ))}
-      </Box>
-      <VStack
-        height={"600px"}
-        // justifyContent="flex-end"
-        padding={4}
-        // backgroundImage="linear-gradient(to right, #65dfc9, #6cdbeb)"
-      >
-        <MotionBox
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-        >
-          <VStack
-            spacing={4}
-            width="400px"
-            borderRadius="md"
-            backgroundColor="white"
-            padding={14}
-          >
-            <Image src="https://img.freepik.com/premium-vector/emoji-raises-his-hands-asks-question_255498-189.jpg" />
-            <Text fontSize="2xl" fontWeight="bold">
-              Ask To Your Colleague
-              <ChatIcon />
-            </Text>
-            <VStack spacing={4} align="stretch">
-              {messages.map((message, index) => (
-                <HStack
-                  key={index}
-                  justifyContent={
-                    message.sender === "user" ? "flex-end" : "flex-start"
-                  }
-                >
-                  {message.sender === "bot" && (
-                    <Avatar size="sm" icon={<FaUser />} />
-                  )}
-                  <Box
-                    borderRadius="md"
-                    backgroundColor={
-                      message.sender === "user" ? "teal.500" : "gray.200"
-                    }
-                    padding={3}
-                    color={message.sender === "user" ? "white" : "black"}
-                  >
-                    {message.text}
-                  </Box>
-                  {message.sender === "user" && (
-                    <Avatar size="sm" icon={<FaUser />} />
-                  )}
-                </HStack>
-              ))}
-            </VStack>
+    <>
+      {" "}
+      <Box>
+        <TableContainer width={"80%"} margin={"auto"}>
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th>EmployId</Th>
+                <Th>Name</Th>
+                <Th>Category</Th>
+                <Th>Action</Th>
+              </Tr>
+            </Thead>
 
-            <InputGroup>
-              <InputLeftElement pointerEvents="none" children={<FaUser />} />
-              <Input
-                placeholder="Recipient ID"
-                type="number"
-                onChange={(e) => setRecieverId(e.target.value)}
-              />
-            </InputGroup>
-            <HStack>
-              <Input
-                flex="1"
-                placeholder="Type a message..."
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-              />
-              <Button colorScheme="teal" onClick={handleSendMessage}>
-                Send
-              </Button>
-            </HStack>
-          </VStack>
-        </MotionBox>
-      </VStack>
-    </ChakraProvider>
+            {data &&
+              data.length > 0 &&
+              data.map((item, index) => (
+                <EmpTable
+                  id={item.empid}
+                  name={item.name}
+                  cat={item.categoryname}
+                  hanldechatid={hanldechatid}
+                />
+              ))}
+          </Table>
+        </TableContainer>
+      </Box>
+      <ChakraProvider>
+        <VStack
+          height={"600px"}
+          // justifyContent="flex-end"
+          padding={4}
+          // backgroundImage="linear-gradient(to right, #65dfc9, #6cdbeb)"
+        >
+          <MotionBox
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <VStack
+              spacing={4}
+              width="400px"
+              borderRadius="md"
+              backgroundColor="white"
+              padding={14}
+            >
+              <Image src="https://img.freepik.com/premium-vector/emoji-raises-his-hands-asks-question_255498-189.jpg" />
+              <Text fontSize="2xl" fontWeight="bold">
+                Ask To Your Colleague
+                <ChatIcon />
+              </Text>
+              <VStack spacing={4} align="stretch">
+                {messages.map((message, index) => (
+                  <HStack
+                    key={index}
+                    justifyContent={
+                      message.sender === "user" ? "flex-end" : "flex-start"
+                    }
+                  >
+                    {message.sender === "bot" && (
+                      <Avatar size="sm" icon={<FaUser />} />
+                    )}
+                    <Box
+                      borderRadius="md"
+                      backgroundColor={
+                        message.sender === "user" ? "teal.500" : "gray.200"
+                      }
+                      padding={3}
+                      color={message.sender === "user" ? "white" : "black"}
+                    >
+                      {message.text}
+                    </Box>
+                    {message.sender === "user" && (
+                      <Avatar size="sm" icon={<FaUser />} />
+                    )}
+                  </HStack>
+                ))}
+              </VStack>
+
+              <InputGroup>
+                <InputLeftElement pointerEvents="none" children={<FaUser />} />
+                <Input
+                value={recieverId}
+                  placeholder="Recipient ID"
+                  type="number"
+                  onChange={(e) => setRecieverId(e.target.value)}
+                />
+              </InputGroup>
+              <HStack>
+                <Input
+                  flex="1"
+                  placeholder="Type a message..."
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                />
+                <Button colorScheme="teal" onClick={handleSendMessage}>
+                  Send
+                </Button>
+              </HStack>
+            </VStack>
+          </MotionBox>
+        </VStack>
+      </ChakraProvider>
+    </>
   );
 };
 
